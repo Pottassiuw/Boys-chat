eaf5c21e9682fe5dabfbedcf967e1a08349cac8c
Pottassiuw <pottassiu@proton.me>
2024-07-01 19:33:37 -0300

    Adding types for the auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -33,4 +33,29 @@ export const authOptions: NextAuthOptions = {
       clientSecret: getGoogleCredentials().clientSecret,
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.picture = token.picture;
+      }
+    },
+  },
 };
