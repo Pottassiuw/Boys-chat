841b1907b47d07d2601e1803941130eaeac15466
Pottassiuw <pottassiu@proton.me>
2024-07-01 14:25:46 -0300

    First Commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,36 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error("GOOGLE_CLIENT_ID is not set");
+  }
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error("GOOGLE_CLIENT_SECRET is not set");
+  }
+  return {
+    clientId,
+    clientSecret,
+  };
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
+};
